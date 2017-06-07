88fb011babd5f6ed3f858b9289b824a48ca0ba85
koala81 <isamaman@gmail.com>
2017-06-07 14:06:24 +0200

    Add files via upload

---
+++ RequeteSQL/DROP PRIMARY KEY_librairie.sql
@@ -0,0 +1,114 @@
+
+
+
+ALTER TABLE Client
+DROP CONSTRAINT pkClient
+
+
+ALTER TABLE Adresse
+DROP CONSTRAINT pkAdresse
+
+
+ALTER TABLE TVA
+DROP CONSTRAINT pkTVA
+
+
+ALTER TABLE Livre
+DROP CONSTRAINT pkLivre
+
+
+ALTER TABLE Auteur
+DROP CONSTRAINT pkAuteur
+
+
+ALTER TABLE Editeur
+DROP CONSTRAINT pkEditeur
+
+
+ALTER TABLE Thematique
+DROP CONSTRAINT pkThematique
+
+
+ALTER TABLE SousThematique
+DROP CONSTRAINT pkSousThematique
+
+
+ALTER TABLE Evenement
+DROP CONSTRAINT pkEvenement
+
+
+ALTER TABLE Livreur
+DROP CONSTRAINT pkLivreur
+
+
+ALTER TABLE Commande
+DROP CONSTRAINT pkCommande
+
+
+ALTER TABLE LigneDeCommande
+DROP CONSTRAINT pkLigneDeCommande
+
+
+ALTER TABLE Employe
+DROP CONSTRAINT pkEmploye
+
+
+ALTER TABLE Parametre
+DROP CONSTRAINT pkParametre
+
+
+ALTER TABLE Avis
+DROP CONSTRAINT pkAvis
+
+
+ALTER TABLE Avis
+DROP CONSTRAINT pkAvis
+
+
+ALTER TABLE Genre
+DROP CONSTRAINT pkGenre
+
+
+ALTER TABLE Genre
+DROP CONSTRAINT pkGenre
+
+
+ALTER TABLE Redaction
+DROP CONSTRAINT pkRedaction
+
+
+ALTER TABLE Redaction
+DROP CONSTRAINT pkRedaction
+
+
+ALTER TABLE Concerne
+DROP CONSTRAINT pkConcerne
+
+
+ALTER TABLE Concerne
+DROP CONSTRAINT pkConcerne
+
+
+ALTER TABLE Presentation
+DROP CONSTRAINT pkPresentation
+
+
+ALTER TABLE Presentation
+DROP CONSTRAINT pkPresentation
+
+
+ALTER TABLE Theme
+DROP CONSTRAINT pkTheme
+
+
+ALTER TABLE Theme
+DROP CONSTRAINT pkTheme
+
+
+ALTER TABLE Invitation
+DROP CONSTRAINT pkInvitation
+
+
+ALTER TABLE Invitation
+DROP CONSTRAINT pkInvitation
+
