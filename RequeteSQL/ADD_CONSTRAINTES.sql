75781fbd9ebbc393bd62d6809feec3989716e659
icem0re <icem0re@users.noreply.github.com>
2017-06-07 14:01:41 +0200

    Create ADD_CONSTRAINTES.sql

---
+++ RequeteSQL/ADD_CONSTRAINTES.sql
@@ -0,0 +1,30 @@
+
+
+/**
+* TABLE Client
+*/
+ALTER TABLE Client ADD CONSTRAINT unLoginClient UNIQUE(loginClient);
+ALTER TABLE Client ADD CONSTRAINT unEmailClient UNIQUE(emailClient);
+
+ALTER TABLE Client ADD CONSTRAINT dfActiviteClient DEFAULT 1 FOR activiteClient;
+
+/**
+* TABLE Employe
+*/
+ALTER TABLE Employe ADD CONSTRAINT unLoginEmploye UNIQUE(loginEmploye);
+ALTER TABLE Employe ADD CONSTRAINT unEmailEmploye UNIQUE(emailEmploye);
+
+
+/**
+* TABLE SousThematique
+*/
+ALTER TABLE SousThematique ADD CONSTRAINT unThematique UNIQUE(nomThematique, nomSousThematique);
+
+
+/**
+* TABLE Evenement
+*/
+ALTER TABLE Evenement ADD CONSTRAINT unCodePromo UNIQUE(codePromoEvenement);
+
+
+
