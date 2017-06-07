c3136bb2958fd2db5cd0dfd68100286999a264b1
WhYn0ot <gaillard.j88@gmail.com>
2017-06-07 14:40:52 +0200

    Add files via upload

---
+++ DROP_TABLE.sql
@@ -0,0 +1,23 @@
+USE Librairie;
+
+DROP TABLE Client;
+DROP TABLE Adresse;
+DROP TABLE Livreur;
+DROP TABLE Commande;
+DROP TABLE TVA;
+DROP TABLE Livre;
+DROP TABLE Thematique;
+DROP TABLE LigneDeCommande;
+DROP TABLE Auteur;
+DROP TABLE Editeur;
+DROP TABLE SousThematique;
+DROP TABLE Employe;
+DROP TABLE Evenement;
+DROP TABLE Parametre;
+DROP TABLE Concerne;
+DROP TABLE Redaction;
+DROP TABLE Invitation;
+DROP TABLE Presentation;
+DROP TABLE Theme;
+DROP TABLE Genre;
+DROP TABLE Avis;
