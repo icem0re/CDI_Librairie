84e3e523637e5c673e1810798e735727e7419772
gregann <rustyryan14@gmail.com>
2017-06-08 17:08:56 +0200

    Add files via upload

---
+++ RequeteSQL/ADD_LIGNEDECOMMANDE.sql
@@ -0,0 +1,8 @@
+use Librairie
+
+INSERT INTO LigneDeCommande (numCommande,isbnLivre,quantiteLigneDeCommande,PrixUnitaireHTLigneDeCommande,TVALigneDeCommande,reductionLigneDeCommande)
+
+VALUES(1,'0375705106',2,24.99,5.5,0),
+	(2,'2253111465',1,7.50,5.5,0),
+	(3,'2290317152',1,10.9,5.5,5.0),
+	(4,'9782253083177',2,21.75,5.5,NULL)
